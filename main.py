7b7b8653a537510f1ecb76f24c607d54b485d134
y0ast <joost@joo.st>
2013-11-07 11:50:15 +0100

    clean up stats and add more arguments

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from preProcess import PreProcess
 from index import Index
 from retrieval import Retrieval
+from statistics import Statistics
 import cPickle as pickle
 import glob
 import argparse
@@ -16,22 +17,29 @@ def loadPickle(fname):
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-n", "--nopickle", help="don't use the saved (pickle) preprocessed index", action="store_true")
+	parser.add_argument("-s", "--statistics", help="Print statistics about the index", action="store_true")
+	parser.add_argument("-r","--retrieval", help="Specify the retrieval algorithm", default="TFIDF")
 	args = parser.parse_args()
 
 	files = glob.glob('collection/*.txt')
-	words = PreProcess(files) if args.nopickle else loadPickle('words.p')
+	documents = PreProcess(files) if args.nopickle else loadPickle('documents.p')
 	index = Index() if args.nopickle else loadPickle('index.p')
 
-	if not words.tokens: 
-		words.tokenize()
-		words.normalize()
-		words.stem()
-		pickle.dump(words,open('words.p',"wb"))
+	if not documents.tokens: 
+		documents.tokenize()
+		documents.normalize()
+		documents.stem()
+		pickle.dump(documents,open('documents.p',"wb"))
 	if not index.index:
-		index.createIndex(words.tokens)
+		index.createIndex(documents.tokens)
 		pickle.dump(index,open('index.p',"wb"))
 
-	ret = Retrieval(index)
-	print ret.BM25([u'a'])
+	if args.statistics:
+		stats = Statistics()
+		stats.getStatistics(documents,index)
+
+	if args.retrieval.lower() == "tfidf":
+		ret = Retrieval(index)
+		print ret.TFIDF([u'sustainable',u'ecosystems'])
 
 
--- statistics.py
+++ statistics.py
@@ -1,31 +1,8 @@
-from preProcess import PreProcess
-from index import Index
-import cPickle as pickle
-
-if __name__ == "__main__":
-	words = PreProcess()
-	try:
-		with open('tokens.p') as tokenfile:
-			words.tokens = pickle.load(tokenfile)
-	except IOError:
-		words.tokenize()
-		# words.normalize()
-		# words.stem()
-		pickle.dump(words.tokens,open("tokens.p","wb"))
-
-	index = Index()
-	try:
-		with open('index.p') as indexfile:
-			index.index = pickle.load(indexfile)
-	except IOError:
-		index.createIndex(words.tokens)
-		pickle.dump(index.index,open("index.p","wb"))
-
-	
-
-	print "Total number of tokens: %i" % sum(map(len,words.tokens.values()))
-	print "Total number of unique tokens %i" % len(index.index)
-	print "Total number of occurences of 'of' %i" % sum(index.index["of"].values())
+class Statistics():
+	def getStatistics(self,documents,index):
+		print "Total number of tokens: %i" % sum(map(len,documents.tokens.values()))
+		print "Total number of unique tokens %i" % len(index.index)
+		print "Total number of occurences of 'of' %i" % sum(index.index["of"].values())
 	
 
 
