62e6b727cfb4dd5c3aadab8bb95cc797f0bb53c4
bennokr <bennokr@gmail.com>
2013-11-13 17:48:56 +0100

    alles outputten

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-n", "--nopickle", help="don't use the saved (pickle) preprocessed index", action="store_true")
 	parser.add_argument("-s", "--statistics", help="Print statistics about the index", action="store_true")
-	parser.add_argument("-r","--retrieval", help="Specify the retrieval algorithm", default="TFIDF")
+	parser.add_argument("-r","--retrieval", help="Specify the retrieval algorithm")
+	parser.add_argument("-q","--query", help="Query string in the format <queryid> term1 term2 ... termn")
 	args = parser.parse_args()
 
 	files = glob.glob('collection/*.txt')
@@ -41,18 +42,26 @@ if __name__ == "__main__":
 		stats = Statistics()
 		stats.getStatistics(documents,index)
 
-	if args.retrieval.lower() == "tfidf":
-		ret = Retrieval(index)
-		with open(args.retrieval+'.txt', 'w') as f:
-			# run query number 6
-			docScores = ret.TFIDF('sustainable ecosystems'.split())
-			for rank, (doc, score) in enumerate(sorted(docScores.iteritems(), key=operator.itemgetter(1),reverse=True)):
-				f.write("6 0 {0} {1} {2} {3}\n".format(doc, rank+1, score, args.retrieval))
+	ret = Retrieval(index)
+	# default scoring and queries
+	retrieval = {"tfidf":ret.TFIDF, "bm25": ret.BM25}
+	# retrieval = {"tfidf":ret.TFIDF, "bm25 k b=": (lamda x: ret.BM25(x, k, b)}
+	queries = {6: 'sustainable ecosystems', 7: 'air guitar textile sensors'}
+	fname = 'out'
 
-			# run query number 7
-			docScores = ret.TFIDF('air guitar textile sensors'.split())
-			for rank, (doc, score) in enumerate(sorted(docScores.iteritems(), key=operator.itemgetter(1), reverse=True)):
-				f.write("7 0 {0} {1} {2} {3}\n".format(doc, rank+1, score, args.retrieval))
+	if args.retrieval and args.retrieval.lower() in retrieval.keys():
+		retrieval = {args.retrieval.lower() : retrieval[args.retrieval.lower()]}
+		fname = args.retrieval
+	if args.query:
+		queries = {args.query[0] : args.query[2:]}
+
+	with open(fname + '.txt', 'w') as f:
+		for retname, retrieve in retrieval.items():
+			for queryid, query in queries.items():
+				docScores = retrieve(query.split())
+				sortedScores = enumerate(sorted(docScores.iteritems(), key=operator.itemgetter(1),reverse=True))
+				for rank, (doc, score) in sortedScores:
+					f.write("{0} 0 {1} {2} {3} {4}\n".format(queryid, doc, rank+1, score, retname))
 
 
 
